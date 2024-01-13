8ecd9ca29b7d114c099668d8d635f270d13c5f9c
CJI3751 <99936049+CJI3751@users.noreply.github.com>
2024-01-13 20:38:29 +0800

    Update hw3.py

--- homework/hw3.py
+++ homework/hw3.py
@@ -1,1 +1,19 @@
-print('hw3')
+def perm(n):
+    def permNext(n, p, used):
+        if len(p) == n:
+            result.append(p.copy())
+            return
+        for x in range(n):
+            if not used[x]:
+                p.append(x)
+                used[x] = True
+                permNext(n, p, used)
+                used[x] = False
+                p.pop()
+
+    result = []
+    permNext(n, [], [False] * n)
+    return result
+  
+print(perm(2))
+print(perm(3)) 
